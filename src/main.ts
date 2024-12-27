165b132c3f777f93e7bc9ba75db1f8e4712fda76
Mun Kit <mk@Lohs-MacBook-Pro.local>
2024-12-27 16:05:06 +0800

    enable api versioning

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import helmet from 'helmet';
+import { VersioningType } from '@nestjs/common';
+import { DEFAULT_APP_VERSION } from 'src/app.constant';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
+
   app.enableCors({
     // TODO: use environment variable values for origin
     origin: ['localhost:3001'], // e.g. ['https://frontend.com', 'https://another-frontend.com']
@@ -15,6 +18,11 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: DEFAULT_APP_VERSION,
+  });
+
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
