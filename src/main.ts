54b84773503cee8c7bfd592f67a5563f07ad651b
Mun Kit <mk@Lohs-MacBook-Pro.local>
2024-12-27 15:39:06 +0800

    implement helmet, cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import helmet from 'helmet';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.enableCors({
+    // TODO: use environment variable values for origin
+    origin: ['localhost:3001'], // e.g. ['https://frontend.com', 'https://another-frontend.com']
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
